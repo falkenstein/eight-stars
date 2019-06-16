1eb6da3b2637869f4667465b90e0827c1739e458
falkenstein <josefhertl@gmail.com>
2019-06-16 21:58:12 +0200

    Skill optimization

--- src/test/main/kotlin/cz/tadea/skill/SkillReaderTest.kt
+++ src/test/main/kotlin/cz/tadea/skill/SkillReaderTest.kt
@@ -12,6 +12,13 @@ class SkillReaderTest {
     fun test() {
         val descs = skillReader.read()
         val grandMasters = descs.flatMap { it.grandmaster }.groupingBy { it }.eachCount().toList().sortedBy { it.second }.toMap()
-        println(grandMasters)
+        val masters = descs.flatMap { it.master }.groupingBy { it }.eachCount().toList().sortedBy { it.second }.toMap()
+        val experts = descs.flatMap { it.expert }.groupingBy { it }.eachCount().toList().sortedBy { it.second }.toMap()
+        val countBySkill = mutableMapOf<SkillType, Int>()
+        SkillType.values().forEach { countBySkill.put(it, 0) }
+        grandMasters.forEach { countBySkill.put(it.key, countBySkill.get(it.key)!! + it.value) }
+        masters.forEach { countBySkill.put(it.key, countBySkill.get(it.key)!! + it.value) }
+        experts.forEach { countBySkill.put(it.key, countBySkill.get(it.key)!! + it.value) }
+        println(countBySkill.toList().sortedBy { it.second }.toMap())
     }
 }
